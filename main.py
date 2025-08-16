616653095547bf36b7e72cafb500c563aecde864
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2025-08-16 04:10:27 -0300

    Some comments removed

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import list
 import api_requests as api
 from utils import Decorators as dec
 
-# api.requests_examples()
-# list.list_examples()
-# oop.opp_examples()
-
+api.requests_examples()
+list.list_examples()
+oop.opp_examples()
 dec.hello()
